6ced30273f1f5d3d1c42c46cd225b5055c594186
hozalice <hozalice65@gmail.com>
2024-05-17 16:04:00 +0400

    sprint1_002748

---
+++ src/mg/itu/prom16/spring/FrontServlet.java
@@ -0,0 +1,109 @@
+package mg.itu.prom16.spring;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import mg.itu.prom16.annotations.*;
+
+public class FrontServlet extends HttpServlet {
+    private boolean checked = false;
+    private final List<String> listeControllers = new ArrayList<>();
+    private final Set<String> verifiedClasses = new HashSet<>();
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        scanControllers(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    private synchronized void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>FrontController</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>URL actuelle :</h1>");
+            out.println("<p>" + request.getRequestURL() + "</p>");
+
+            if (!checked) {
+                ServletConfig config = getServletConfig();
+                scanControllers(config);
+                checked = true;
+            }
+
+            out.println("<h2>Liste des contrôleurs annotés avec @Annotation_controlleur :</h2>");
+            for (String controller : listeControllers) {
+                out.println("<p>" + controller + "</p>");
+            }
+
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    private void scanControllers(ServletConfig config) {
+        String controllerPackage = config.getInitParameter("controller-package");
+        System.out.println("Scanning package: " + controllerPackage);
+
+        // Scanner les classes du package donné dans WEB-INF/classes
+        try {
+            String path = "WEB-INF/classes/" + controllerPackage.replace('.', '/');
+            File directory = new File(getServletContext().getRealPath(path));
+            if (directory.exists()) {
+                scanDirectory(directory, controllerPackage);
+            } else {
+                System.out.println("Directory does not exist: " + directory.getAbsolutePath());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void scanDirectory(File directory, String packageName) {
+        System.out.println("Scanning directory: " + directory.getAbsolutePath());
+
+        for (File file : directory.listFiles()) {
+            System.out.println("Processing file: " + file.getName());
+
+            if (file.isDirectory()) {
+                scanDirectory(file, packageName + "." + file.getName());
+            } else if (file.getName().endsWith(".class")) {
+                String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                try {
+                    Class<?> clazz = Class.forName(className);
+                    if (clazz.isAnnotationPresent(Annotation_controlleur.class)
+                            && !verifiedClasses.contains(clazz.getName())) {
+                        Annotation_controlleur annotation = clazz.getAnnotation(Annotation_controlleur.class);
+                        listeControllers.add(clazz.getName() + " (" + annotation.value() + ")");
+                        verifiedClasses.add(clazz.getName());
+                        System.out.println("Added controller: " + clazz.getName());
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}

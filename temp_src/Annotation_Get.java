eecc14de6e43b50bdada5a52f7d55500bb94fa31
hozalice <hozalice65@gmail.com>
2024-11-12 21:07:08 +0200

    sprint10

--- temp_src/Annotation_Get.java
+++ temp_src/Annotation_Get.java
@@ -1,9 +1,12 @@
 package mg.itu.prom16.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Annotation_Get {
-    String value();
+    String value() default ""; // URL associée à la méthode GET
 }

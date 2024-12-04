3462113423b07f4dba560ad89dfc4e44ee4746bf
hozalice <hozalice65@gmail.com>
2024-12-04 13:38:38 +0200

    sprint13_2748

---
+++ temp_src/VerbAction.java
@@ -0,0 +1,30 @@
+package mg.itu.prom16.map;
+
+public class VerbAction {
+    private String methodeName;
+    private String verb;
+
+    public VerbAction(String methodeName, String verb) {
+        this.methodeName = methodeName;
+        this.verb = verb;
+    }
+
+    public String getMethodeName() {
+        return methodeName;
+    }
+
+    public void setMethodeName(String methodeName) {
+        this.methodeName = methodeName;
+    }
+
+    public String getVerb() {
+        return verb;
+    }
+
+    public void setVerb(String verb) {
+        this.verb = verb;
+    }
+
+    
+   
+}
